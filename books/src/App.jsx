3bb64d8dafcfc34b969fa250dab9e2897b855fb7
divyamprabhudessai <divyam.desai@kalvium.community>
2024-01-29 11:55:49 +0530

    added nav bar and integrated API

---
+++ books/src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
+import {Link, Route,Routes} from 'react-router-dom'
+import Form from './Form';
+
+
+function App() {
+  const [myData, setMyData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  <Routes>
+    <Route path='/form' element={<Form/>}/>
+  </Routes>
+
+  useEffect(() => {
+    axios.get("https://reactnd-books-api.udacity.com/books", {
+      headers: {
+        'Authorization': 'authorization'
+      }
+    })
+      .then(res => {
+        const bookData = res.data
+        console.log(bookData)
+        setMyData(bookData.books)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  const filteredBooks = myData.filter(book =>
+    book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <>
+      <nav>
+        <h1 id='title'>Kalvium Books</h1>
+        <input
+          type="search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button id='register' onClick={<Link to="/form"/>}>Register</button>
+      </nav>
+      <div className='books'>
+        {filteredBooks.map((book) => (
+          <div key={book.id} className='onebook'>
+            <img src={book.imageLinks.smallThumbnail} alt="" />
+            <h3>{book.title}</h3>
+            <p>{book.authors.map((authors)=> <div>{authors}</div>)}</p>
+            <p>Free</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ books/src/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form(){
+    return(
+        <>
+            <p>HELLO WORLD</p>
+        </>
+    )
+}
+
+export default Form
